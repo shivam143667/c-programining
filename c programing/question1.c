092218e9635434c842c17694bd3da29126bd564f
shivam143667 <shivamkeshriajaykeshri@gmail.com>
2025-11-12 14:45:48 +0530

    Delete c programing directory

--- c programing/question1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    float centigrade,fahrenheit;
-    printf("enter the temperature in fahrenheit");
-    scanf("%f",&centigrade);
-    fahrenheit=(9*centigrade)/5+32;
-    printf("%d",&fahrenheit);
-
-}
